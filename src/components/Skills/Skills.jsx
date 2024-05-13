c2b55690c85e50d781fa735adafccffdd15c0025
Gihan Bandara <63912749+Gihan1329@users.noreply.github.com>
2024-05-13 15:52:39 +0530

    Cmplete the skills section

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Skills.css";
+import { SKILLS } from "../../utils/data";
+import SkillsCard from "./SkillCard/SkillCard";
+import SkillInfoCard from "./SkillInfoCard/SkillInfoCard";
+
+const Skills = () => {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+  const handleSelectedSkill = (data) => {
+    setSelectedSkill(data);
+  };
+  return (
+    <section className="skills-container">
+      <h5>Technical Proviciency</h5>
+
+      <div className="skills-content">
+        <div className="skills">
+          {SKILLS.map((item) => (
+            <SkillsCard
+              key={item.title}
+              iconUrl={item.icon}
+              title={item.title}
+              isActive={selectedSkill.title === item.title}
+              onClick={() => handleSelectedSkill(item)}
+            />
+          ))}
+        </div>
+        <div className="skills-info">
+          <SkillInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
---
+++ src/utils/data.js
@@ -0,0 +1,76 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "./assets/images/programming.png",
+        skills: [
+            { skill: "HTML5", percentage: "90%" },
+            { skill: "CSS3", percentage: "90%" },
+            { skill: "JavaScript", percentage: "75%" },
+            { skill: "React.js", percentage: "80%" },
+
+        ],
+    },
+    {
+        title: "Backend",
+        icon: "./assets/images/coding.png",
+        skills: [
+            { skill: "Node.js", percentage: "85%" },
+            { skill: "Express.js", percentage: "80%" },
+            { skill: "MongoDB", percentage: "75%" },
+            { skill: "SQL", percentage: "70%" }
+        ]
+    },
+    {
+        title: "Tools",
+        icon: "./assets/images/settings.png",
+        skills: [
+            { skill: "Visual Studio", percentage: "80%" },
+            { skill: "Git & GitHub", percentage: "75%" },
+
+        ]
+    },
+    {
+        title: "Soft Skills",
+        icon: "./assets/images/soft-skills.png",
+        skills: [
+            { skill: "Communication", percentage: "95%" },
+            { skill: "Problem Solving", percentage: "90%" },
+            { skill: "Teamwork", percentage: "85%" },
+            { skill: "Time Management", percentage: "80%" }
+        ]
+    }
+
+];
+
+export const PROJECTS = [
+    {
+        title: "Pet Care Mobile Application",
+        projectUrl: "https://github.com/Gihan1329/Pet-Care-Application",
+        discription: "PetCare is a user-friendly mobile application designed for pet owners and caregivers. The app simplifies the process of finding trustworthy pet care services. Pet owners can easily connect with caregivers, providing essential details about their pets, and caregivers can find job opportunities seamlessly.",
+    },
+    {
+        title: "Car Rental Website",
+        projectUrl: "https://github.com/Gihan1329/Car-Rental-Website",
+        discription: "This is a web application for managing vehicle rentals. Users can browse available vehicles, submit rental requests, and admins can manage vehicle listings, rental requests, and user accounts. Built using PHP, MySQL, and HTML/CSS, this project offers a convenient platform for renting vehicles online.",
+    },
+    {
+        title: "Outer Clove Restuarant Website",
+        projectUrl: "https://github.com/Gihan1329/The-Outer-Clove-Restaurant-Web-Application",
+        discription: "Outer Clove restaurant's web application enhances customer and worker experience with a sleek, user-friendly layout, influenced by successful competitors' websites.",
+    },
+    {
+        title: "LifeDrop Mobile Application",
+        projectUrl: "https://github.com/Gihan1329/LifeDrop-React-Native-Mobile-Application",
+        discription: "Life Drop is a Sri Lankan Centralized Blood Donor Application that streamlines blood donation processes, offers features like donor registration, recipient management, and real-time communication, and prioritizes data privacy, security, and scalability.",
+    },
+    {
+        title: "Unique Password Generator Mobile Application",
+        projectUrl: "https://github.com/Gihan1329/PasswordGenerator-React-Native",
+        discription: "This is a simple application designed to create unique passwords for users.",
+    },
+    {
+        title: "API that handle ticket Booking",
+        projectUrl: "https://github.com/Gihan1329/Sri-Lankan-Railway-SOC-Ticketing-System-API",
+        discription: "The Sri Lankan Railway SOC Ticketing System API provides a comprehensive set of endpoints for interacting with the ticketing system's functionalities. Designed with a RESTful architecture, the API enables seamless integration with external entities such as travel agents and third-party applications.",
+    }
+]
